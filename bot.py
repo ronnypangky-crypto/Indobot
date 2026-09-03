6ac024c9388c4fe9b17b063d9fb19a1396b5b2f6
ronnypangky-crypto <ronny.pangky@gmail.com>
2026-09-03 10:57:05 +0700

    Update configuration values for trading parameters

--- bot.py
+++ bot.py
@@ -11,15 +11,15 @@ TG_CHAT_ID    = os.environ.get("TELEGRAM_CHAT_ID", "")
 API_KEY       = os.environ.get("INDODAX_API_KEY", "")
 API_SECRET    = os.environ.get("INDODAX_SECRET_KEY", "")
 
-TOP_N_PAIRS   = int(os.environ.get("TOP_N_PAIRS",   "800"))
-MAX_TRADES    = int(os.environ.get("MAX_TRADES",    "10"))
+TOP_N_PAIRS   = int(os.environ.get("TOP_N_PAIRS",   "400"))
+MAX_TRADES    = int(os.environ.get("MAX_TRADES",    "20"))
 TP_PCT        = float(os.environ.get("TP_PCT",      "25"))
 TRAIL_PCT     = float(os.environ.get("TRAIL_PCT",   "10"))
 TIME_STOP_HR  = int(os.environ.get("TIME_STOP_HR",  "0"))   # 0 = hold seumur hidup
 BLACKLIST_HR  = int(os.environ.get("BLACKLIST_HR",  "2"))
 SCAN_INTERVAL = int(os.environ.get("SCAN_INTERVAL", "10"))
 MIN_SIGNALS   = int(os.environ.get("MIN_SIGNALS",   "4"))
-MIN_VOL_IDR   = float(os.environ.get("MIN_VOL_IDR",  "20000000"))  # min volume 20jt
+MIN_VOL_IDR   = float(os.environ.get("MIN_VOL_IDR",  "30000000"))  # min volume 30jt
 MIN_PRICE     = float(os.environ.get("MIN_PRICE",    "150"))          # min harga Rp 150
 MAX_SPREAD    = float(os.environ.get("MAX_SPREAD",   "1.5"))         # max spread 1.5%
 WARN_STOP_HR  = int(os.environ.get("WARN_STOP_HR",  "24"))          # warning sebelum time stop
